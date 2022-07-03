c88d4bc00d9e4a22de200f2654af2d05adfede21
JongSeung Kim <88412551+jngsng@users.noreply.github.com>
2022-07-03 15:56:32 +0000

    get pages

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("test")
+import requests
+from bs4 import BeautifulSoup
+
+# method / function inside of object
+
+saramin_result = requests.get("https://www.saramin.co.kr/zf_user/search/recruit?searchType=search&searchword=python&company_cd=0%2C1%2C2%2C3%2C4%2C5%2C6%2C7%2C9%2C10&keydownAccess=&job_type=1&panel_type=&search_optional_item=y&search_done=y&panel_count=y&recruitPage=2&recruitSort=relation&recruitPageCount=40&inner_com_type=&show_applied=&quick_apply=&except_read=&ai_head_hunting=")
+
+saramin_soup = BeautifulSoup(saramin_result.text, "html.parser")
+
+pagination = saramin_soup.find("div",{"class":"pagination"})
+
+pages = pagination.find_all("a")
+spans = []
+
+for page in pages:
+  spans.append(page.find("span"))
+
+print(spans[0:-1])
