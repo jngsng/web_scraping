e05bba07f7a95bbfccc1c94ad64da3b79fb5c548
JongSeung Kim <88412551+jngsng@users.noreply.github.com>
2022-07-05 12:31:50 +0000

    request function

--- main.py
+++ main.py
@@ -1,21 +1,5 @@
-import requests
-from bs4 import BeautifulSoup
+from saramin import extract_saramin_pages, extract_saramin_jobs
 
-# method / function inside of object
+last_saramin_pages = extract_saramin_pages()
 
-saramin_result = requests.get("https://www.saramin.co.kr/zf_user/search/recruit?searchType=search&searchword=python&company_cd=0%2C1%2C2%2C3%2C4%2C5%2C6%2C7%2C9%2C10&keydownAccess=&job_type=1&panel_type=&search_optional_item=y&search_done=y&panel_count=y&recruitPage=2&recruitSort=relation&recruitPageCount=40&inner_com_type=&show_applied=&quick_apply=&except_read=&ai_head_hunting=")
-
-saramin_soup = BeautifulSoup(saramin_result.text, "html.parser")
-
-pagination = saramin_soup.find("div",{"class":"pagination"})
-
-links = pagination.find_all("a")
-pages = []
-
-for link in links[:-1]:
-  pages.append(int(link.string))
-
-max_page = pages[-1]
-
-for n in range(max_page):
-  print(f"recruitPage={n+1}")
+extract_saramin_jobs(last_saramin_pages)
--- saramin.py
+++ saramin.py
@@ -3,15 +3,17 @@ from bs4 import BeautifulSoup
 
 # method / function inside of object
 
-SARAMIN_URL = "https://www.saramin.co.kr/zf_user/search/recruit?searchType=search&searchword=python&company_cd=0%2C1%2C2%2C3%2C4%2C5%2C6%2C7%2C9%2C10&keydownAccess=&job_type=1&panel_type=&search_optional_item=y&search_done=y&panel_count=y&" + "recruitPage=1" + "&recruitSort=relation&recruitPageCount=40&inner_com_type=&show_applied=&quick_apply=&except_read=&ai_head_hunting=&mainSearch=n"
+URL1 = "https://www.saramin.co.kr/zf_user/search/recruit?searchType=search&searchword=python&company_cd=0%2C1%2C2%2C3%2C4%2C5%2C6%2C7%2C9%2C10&keydownAccess=&job_type=1&panel_type=&search_optional_item=y&search_done=y&panel_count=y&"
+
+URL2 = "&recruitSort=relation&recruitPageCount=40&inner_com_type=&show_applied=&quick_apply=&except_read=&ai_head_hunting=&mainSearch=n"
 
 
 def extract_saramin_pages():
-  saramin_result=requests.get(SARAMIN_URL)
+  result=requests.get(URL1+URL2)
   
-  saramin_soup = BeautifulSoup(saramin_result.text, "html.parser")
+  soup = BeautifulSoup(result.text, "html.parser")
   
-  pagination = saramin_soup.find("div",{"class":"pagination"})
+  pagination = soup.find("div",{"class":"pagination"})
   
   links = pagination.find_all("a")
   pages = []
@@ -20,3 +22,10 @@ def extract_saramin_pages():
     pages.append(int(link.string))
   
   max_page = pages[-1]
+  return(max_page)
+
+
+def extract_saramin_jobs(last_page):
+  for page in range(last_page):
+    result = requests.get(f"{URL1}recruitPage={page+1}{URL2}")
+    print(result.status_code)
